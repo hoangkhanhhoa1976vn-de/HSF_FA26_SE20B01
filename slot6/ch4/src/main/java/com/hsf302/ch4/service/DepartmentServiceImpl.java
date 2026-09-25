abf37eb2f1a64bd045cff6078d2bb210d5106d8f
hoangkhanhhoa <hoangkhanhhoa1976vn@gmail.com>
2026-09-25 15:10:12 +0700

    feat(repository): add repositories and service layer skeleton
    Refs: TODO 4

---
+++ slot6/ch4/src/main/java/com/hsf302/ch4/service/DepartmentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.hsf302.ch4.service;
+
+import com.hsf302.ch4.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+}
